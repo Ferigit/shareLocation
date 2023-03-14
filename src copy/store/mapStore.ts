c722332dc27bf1a164faf822abc908a6de632ed8
Ferigit <faramarz.razmi@gmail.com>
2023-03-14 04:43:37 +0330

    clean code

--- src copy/store/mapStore.ts
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-import { ILocation, IMap } from "types/types";
-
-export const useMapStore = create<IMap>((set, get) => ({
-  location: { lat: 21, lng: 23 },
-  updateLocation: (newLocation: ILocation) =>
-    set((state) => ({ ...state, location: newLocation })),
-}));
